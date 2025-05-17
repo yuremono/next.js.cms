f71c865823b42e7b9abdd779e34ab2de745e9b62
yuremono <5alviaofficinali50@gmail.com>
2025-05-17 20:10:30 +0900

    UI変更: primaryカラーを紺色に変更・セクション名をtext-smに変更

--- components/SortableSections.tsx
+++ components/SortableSections.tsx
@@ -79,7 +79,7 @@ export function SortableSections({
 					<div className="flex items-center justify-between">
 						<div className="flex items-center">
 							{getSectionIcon(section)}
-							<span className="text-base">
+							<span className="text-sm">
 								{getSectionTitle(section)}
 							</span>
 						</div>
