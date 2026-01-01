2b1ce3addc44cadf4414b120df6e1cf18d55743f
yuremono <5alviaofficinali50@gmail.com>
2026-01-01 21:17:50 +0900

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" suppressHydrationWarning>
-      <head />
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+      </head>
       <body>
         {/* CSS読み込み順序: variables.css → custom.css */}
         <VariablesLoader />
