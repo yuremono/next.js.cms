8577cab40467e8e687272c23b38d1756b980d256
yuremono <5alviaofficinali50@gmail.com>
2026-01-01 21:49:42 +0900

    deploy

--- lib/tailwind-trigger.tsx
+++ lib/tailwind-trigger.tsx
@@ -6,7 +6,7 @@
  */
 
 // データベースから取得したクラス
-const rareClasses = "bg-no-repeat bg-contain bg-bottom";
+const rareClasses = "bg-no-repeat bg-contain bg-bottom text-[1.5em]";
 
 // すべてを統合したトリガーストラップ
 export const tailwindTrigger = rareClasses;
